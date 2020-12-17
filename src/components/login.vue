4ecbf128d4e9d369f1239498f98c76f24f7d539c
ivybsugarman <840464844@qq.com>
2020-12-17 12:40:59 +0800

    scatter做登录功能完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,19 +3,27 @@
         
         <el-form class="login-form" label-position="left" label-width="80px" :model="format">
             <h1>用户登录</h1>
-            <el-form-item label="用户名">
-                <el-input v-model="format.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input v-model="format.password"></el-input>
-            </el-form-item>
-            <el-button class="login-btn" type="primary" @click="handleLogin">登录</el-button>
-
+            <h2>请确保您在本地下载Scatter电子钱包，并且在电子钱包中导入了麒麟测试链的PKI。</h2>
+            <h2>{{loginMessage}}</h2>
         </el-form>
     </div>
 </template>
 
 <script>
+import ScatterJS from '@scatterjs/core';
+import ScatterEOS from '@scatterjs/eosjs';
+import Eos from 'eosjs';
+
+ScatterJS.plugins( new ScatterEOS() );
+
+const network = ScatterJS.Network.fromJson({
+    blockchain:'eos',
+    chainId:'5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
+    host:'api-kylin.eoslaomao.com',
+    port:443,
+    protocol:'https'
+});
+
 
 export default {
 //import引入的组件
@@ -26,19 +34,50 @@ return {
         username: 111,
         password: 111
     },
+    loginMessage:null
 
 };
 },
 //方法集合
 methods: {
+    loadScatter(){
+
+    },
     handleLogin(){
         this.$router.push("/home")
     }
 },
 //生命周期-创建完成( 可以访问当前this实例)
-created() {},
+created() {
+    
+},
 //生命周期-挂载完成(可以访问D0M元素)
-mounted() {},
+mounted() {
+    ScatterJS.connect('eduDapp', {network}).then(connected => {
+    if(!connected) {
+        this.loginMessage = "未检测到本地安装的scatter"
+        return console.error('no scatter');
+    }
+    const eos = ScatterJS.eos(network, Eos);
+    ScatterJS.login().then(id => {
+        if(!id){
+            this.loginMessage = "登录失败"
+            return console.error('no identity');
+        }else{
+            this.loginMessage = "登录成功"
+        } 
+        const account = ScatterJS.account('eos');
+        const options = {authorization:[`${account.name}@${account.authority}`]};
+        console.log(options);
+        // eos.transfer(account.name, 'safetransfer', '0.0001 EOS', account.name, options).then(res => {
+        //     console.log('sent: ', res);
+        // }).catch(err => {
+        //     console.error('error: ', err);
+        // });
+    });
+});
+
+},
 beforeCreate() {}, //生命周期-创建之前
 beforeMount() {}, //生命周期-挂载之前
 beforeUpdate() {}, //生命周期-更新之前
